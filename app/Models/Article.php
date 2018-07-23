09a0dd8102dbd31436da05121c0b38cb7ce6a6d3
xiao <jx_xiaojin@aliyun.com>
2018-07-24 00:24:27 +0800

    2018年7月24日

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,8 +3,42 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $dates = ['published_at', 'created_at', 'deleted_at'];
+
+    protected $fillable = [
+        'user_id',
+        'last_user_id',
+        'category_id',
+        'title',
+        'subtitle',
+        'slug',
+        'page_image',
+        'content',
+        'meta_description',
+        'is_draft',
+        'is_original',
+        'published_at',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
