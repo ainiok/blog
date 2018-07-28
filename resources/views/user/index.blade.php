6a532d50a8addea7c11e920bafe867367e6c60ac
xiao <jx_xiaojin@aliyun.com>
2018-07-28 23:23:35 +0800

    2018年7月28日

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,5 +3,25 @@
 @section('title',lang('Personal Center'))
 
 @section('content')
-<div>111</div>
+    @include('user.particals.info')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ lang('Recent Discussions') }}</div>
+
+                    @include('user.particals.discussions')
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ lang('Recent Comments') }}</div>
+
+                    @include('user.particals.comments')
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
