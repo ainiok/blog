2612bde6f4c9d860cf246d2f25ebf2ef092fe0b2
xiao <jx_xiaojin@aliyun.com>
2018-08-01 23:37:13 +0800

    update

--- config/blog.php
+++ config/blog.php
@@ -13,16 +13,16 @@ return [
 
     // Meta
     'meta' => [
-        'keywords' => 'PJ Blog,blog,pigjian,laravel,vuejs',
-        'description' => 'Nothing is impossible in PJ Blog'
+        'keywords' => 'Blog,blog,ainiok,laravel,vuejs',
+        'description' => 'vuejs & laravel & blog'
     ],
 
     // Social Share
     'social_share' => [
         'article_share'    => env('ARTICLE_SHARE') ?: true,
         'discussion_share' => env('DISCUSSION_SHARE') ?: true,
-        'sites'            => env('SOCIAL_SHARE_SITES') ?: 'google,twitter,weibo',
-        'mobile_sites'     => env('SOCIAL_SHARE_MOBILE_SITES') ?: 'google,twitter,weibo,qq,wechat',
+        'sites'            => env('SOCIAL_SHARE_SITES') ?: 'google,weibo',
+        'mobile_sites'     => env('SOCIAL_SHARE_MOBILE_SITES') ?: 'google,weibo,qq,wechat',
     ],
 
     // Google Analytics
@@ -34,7 +34,7 @@ return [
     // Article Page
     'article' => [
         'title'       => 'Nothing is impossible.',
-        'description' => 'https://pigjian.com',
+        'description' => 'https://blog.ainiok.com',
         'number'      => 15,
         'sort'        => 'desc',
         'sortColumn'  => 'published_at',
@@ -51,15 +51,11 @@ return [
     'footer' => [
         'github' => [
             'open' => true,
-            'url'  => 'https://github.com/jcc',
+            'url'  => 'https://github.com/ainiok',
         ],
-        'twitter' => [
-            'open' => true,
-            'url'  => 'https://twitter.com/pigjian'
-        ],
-        'meta' => '© PJ Blog 2016. Powered By Jiajian Chan',
+        'meta' => '© Blog 2018. Powered By ainiok',
     ],
 
-    'license' => 'Powered By Jiajian Chan.<br/>This article is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.',
+    'license' => 'Powered By ainiok.<br/>This article is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.',
 
 ];
